6f07e41da411d6599a1f8a5960340e48ca83729f
Daniel Palacios <danpal@tuta.io>
2021-05-05 22:24:43 -0500

    Changed google_speech for gtts. Saves audios on a child folder called 'ru'

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from google_speech import Speech
 import csv
 from os import path
 from retrying import retry
-# from gtts import gTTS
+from gtts import gTTS
 
 f = open('ruswords.csv', newline='', encoding="utf-8")
 reader = csv.reader(f)
@@ -22,8 +22,9 @@ def script(reader):
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=40000)
 def save_word(word, wordForFile):
     print(word)
-    Speech.save(Speech(word, "RU"), + wordForFile + ".mp3")
-
+    tts = gTTS(word, lang="ru")
+    # Speech.save(Speech(word, "RU"), + wordForFile + ".mp3")
+    tts.save( "ru/" + wordForFile + ".mp3")
 
 # print("hello aaaa bbbb cccc".split(" ")[0])
 

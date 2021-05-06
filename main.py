9a97eb8708fbe8a49c0172b41d6ca81f67337d50
Daniel Palacios <danpal@tuta.io>
2021-05-06 18:23:13 -0500

    Added function that downloads the English word/sentence of each Russian word. It works.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from google_speech import Speech
 import csv
 from os import path
 from retrying import retry
@@ -7,15 +6,16 @@ from gtts import gTTS
 f = open('ruswords.csv', newline='', encoding="utf-8")
 reader = csv.reader(f)
 
-
 def script(reader):
     for row in reader:
         word = row[0].split("(")[0].split("/у-")[0].split("/о-")[0].split("/по-")[0].split("/за-")[0].split("/с-")[0]
         wordForFile = word.split("/")[0]  # because files can't have slashes
-        if path.isfile(wordForFile + ".mp3"):  # to not run over the same word twice
+        eng_text = row[1]
+        if path.isfile("ru/" + wordForFile + ".mp3"):  # to not run over the same word twice
             pass
         else:
             save_word(word, wordForFile)
+            savetext_eng(eng_text, wordForFile)
         # pass
 
 
@@ -26,7 +26,13 @@ def save_word(word, wordForFile):
     # Speech.save(Speech(word, "RU"), + wordForFile + ".mp3")
     tts.save( "ru/" + wordForFile + ".mp3")
 
-# print("hello aaaa bbbb cccc".split(" ")[0])
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=40000)
+def savetext_eng(eng_text, wordForFile):
+    print(wordForFile + "_eng")
+    tts = gTTS(eng_text)
+    # Speech.save(Speech(word, "RU"), + wordForFile + ".mp3")
+    tts.save( "eng/" + wordForFile + ".mp3")
 
 script(reader)
 
